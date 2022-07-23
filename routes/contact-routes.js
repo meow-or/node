fd7328fa786590f92919379549144219670af8d9
Pavel <73703906+meow-or@users.noreply.github.com>
2022-07-23 18:45:28 +0500

    рефакторинг, вынесение роутинга

---
+++ routes/contact-routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const Contact = require('../models/contact');
+
+const router = express.Router();
+const createPath = require('../helpers/create-path');
+
+router.get('/contacts', (req, res) => {
+  const title = 'Contacts';
+  Contact
+    .find()
+    .then((contacts) => {
+      res.render(createPath('contacts'), { contacts, title })
+    })
+    .catch((err) => {
+      res.render(createPath('error'), { title: 'Error' })
+    });
+});
+
+module.exports = router;
